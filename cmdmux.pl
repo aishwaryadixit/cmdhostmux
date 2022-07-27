1ff25c3b510dffedb365ea0d48ae85bf563e68e4
Aishwarya Dixit <aishwarya.dixit1995@gmail.com>
2022-07-28 02:07:09 +0530

    Adding file input for reading the list of hosts

--- cmdmux.pl
+++ cmdmux.pl
@@ -2,12 +2,23 @@
 
 use strict;
 use warnings;
+use autodie;
 
 our $VERSION = '0.1';
 
-my @user_at_host = ("$ENV{USER}\@localhost", 'example\@example.com');
-my $command      = 'echo hello';
-my $key          = "$ENV{HOME}/cmdhostmux";
+my $i            = 0;
+my @user_at_host = ();
+
+open my $fh, '<', $ARGV[0];
+
+while (<$fh>) {
+    chomp ($_);
+    @user_at_host[$i] = $_;
+    $i++;
+}
+
+my $command = 'echo "hello from \$USER"';
+my $key     = "$ENV{HOME}/cmdhostmux";
 
 foreach my $user_at_host (@user_at_host) {
     my $out = `ssh -i $key $user_at_host $command`;
